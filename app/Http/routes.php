77cd6c6caf9dead2013279d68f1509588e2e2e6b
CGTAbhay <abhay@cgt.co.in>
2016-02-29 11:43:04 +0530

    Framework strucuture

---
+++ app/Http/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+ */
+
+Route::get('/', function () {
+    return view('master');
+});
+
+post('upload-image', 'GalleryController@uploadImage');
+
+Route::post('auth', 'UserController@checkAuth');
+Route::resource('user', 'UserController');
+Route::resource('gallery', 'GalleryController');
+
+post('delete-single-image', 'GalleryController@deleteSingleImage');
---
+++ resources/assets/js/controllers/userController.js
@@ -0,0 +1,25 @@
+myApp.controller('userController', ['$scope', '$location', 'userModel', function($scope, $location, userModel) {
+    angular.extend($scope, {
+        login: {
+            username: 'abhay@cgt.co.in',
+            password: '12345678'
+        },
+        currentUser:{}
+    });
+
+
+
+    angular.extend($scope, {
+        doLogin: function(loginForm) {
+            var data = {
+                email: $scope.login.username,
+                password: $scope.login.password
+            };
+
+            userModel.doLogin(data).then(function() {
+                $location.path('/dashboard');
+            });
+        },
+        
+    });
+}]);
