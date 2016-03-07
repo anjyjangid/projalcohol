db4689c0a1a9029ffdee35d1ef6c3ba24e15be7a
CGTAnurag <anurag.pareek@cgt.co.in>
2016-03-07 15:21:22 +0530

    Vendor ignore

--- public/templates/users/alcohol/include-css.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
-<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
-<link rel="stylesheet" type="text/css" href="css/jquery.switchButton.css">
-<link rel="stylesheet" type="text/css" href="css/screen-ui.css">
-<link rel="stylesheet" type="text/css" href="css/ui_responsive.css">
