eabf5309eba80a10064a98923f7f9946e4cfe547
CGTAbhay <abhay@cgt.co.in>
2016-05-12 14:56:54 +0530

    FB login by yogesh sir

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,8 @@ Route::get('/', function () {
     return view('frontend');
 });
 
+Route::post('auth/registerfb', 'Auth\AuthController@registerfb');
+
 Route::controller('/auth', 'Auth\AuthController');
 
 Route::controller('/super', 'SuperController');
@@ -216,6 +218,8 @@ Route::controller('/admin', 'Auth\AdminAuthController');
 Route::get('/check', 'UserController@check');
 
 Route::post('/auth', 'UserController@checkAuth');
+
+
 
 Route::get('/loggedUser', 'UserController@loggedUser');
 
--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,8 @@ var AlcoholDelivery = angular.module('AlcoholDelivery', [
 	'ngAnimate',
 	'ngMaterial',
 	'ngMessages',
-	'ngTouch'
+	'ngTouch',
+	'ngFacebook'
 ]);
 
 
@@ -23,7 +24,11 @@ AlcoholDelivery.config(
 		$ocLazyLoadProvider.config({
 			// global configs go here
 		});
-}]);	
+}]);
+
+AlcoholDelivery.config(['$facebookProvider', function($facebookProvider) {
+    $facebookProvider.setAppId('273669936304095').setPermissions(['email','user_friends']);
+}]);
 
 AlcoholDelivery.config(['$controllerProvider', function($controllerProvider) {
   // this option might be handy for migrating old apps, but please don't use it
@@ -41,7 +46,7 @@ AlcoholDelivery.filter('capitalize', function() {
 });
 
 
-AlcoholDelivery.controller('AppController', ['$scope', '$rootScope','$http', function($scope, $rootScope,$http) {
+AlcoholDelivery.controller('AppController', ['$scope', '$rootScope','$http', '$facebook', function($scope, $rootScope,$http,$facebook) {
 
 	$scope.AppController = {};
 	$scope.featuredProduct = [];
@@ -164,6 +169,40 @@ AlcoholDelivery.controller('AppController', ['$scope', '$rootScope','$http', fun
 		return localpro;
 
 	}
+
+
+	// YKB Facebook start //
+	$scope.$on('fb.auth.authResponseChange', function() {
+      $scope.status = $facebook.isConnected();
+      if($scope.status) {
+        $facebook.api('/me?fields=email,name').then(function(user) {
+          //$scope.user = user;
+          //console.log(user);
+          user.fbid = user.id;
+          $http.post('/auth/registerfb',user).success(function(response){
+
+					if(response.success==true)
+					{
+						$scope.user = response.data;
+						$('#login').modal('hide');
+					}
+
+	            }).error(function(data, status, headers) {                            
+	                $scope.login.errors = data;                
+	            });
+
+        });
+      }
+    });
+
+    $scope.loginToggle = function() {
+      if($scope.status) {
+        $facebook.logout();
+      } else {
+        $facebook.login();
+      }
+    };
+    // YKB facebook end //
 
 }]);
 
@@ -448,7 +487,15 @@ AlcoholDelivery.controller('ProfileController',['$scope','$rootScope','$state','
 
 }]);
 
-AlcoholDelivery.controller('PasswordController',['$scope','$rootScope','$state','$http','sweetAlert',function($scope,$rootScope,$state,$http,sweetAlert){
+AlcoholDelivery.controller('PasswordController',['$scope','$rootScope','$state','$http','sweetAlert','UserService',function($scope,$rootScope,$state,$http,sweetAlert,UserService){
+
+	UserService.GetUser().then(
+			
+	    function(result) {
+	    	$scope.currentPasswordHide = result.loginfb;
+	    }
+	);
+
 
 	$scope.password = {
 		current:'',
@@ -1369,7 +1416,7 @@ function ($q, $rootScope, $log) {
 }]);
 
 /* Init global settings and run the app */
-AlcoholDelivery.run(["$rootScope", "appSettings", "catPricing","UserService", "$state" , function($rootScope, settings, catPricing, UserService, $state) {		
+AlcoholDelivery.run(["$rootScope", "appSettings", "catPricing","UserService", "$state", "$window", function($rootScope, settings, catPricing, UserService, $state, $window) {		
 
 	$rootScope.$state = $state; // state to be accessed from view
 
@@ -1408,7 +1455,16 @@ AlcoholDelivery.run(["$rootScope", "appSettings", "catPricing","UserService", "$
 	});
 
 
-
+	(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    $rootScope.$on('fb.load', function() {
+      $window.dispatchEvent(new Event('fb.load'));
+    });
 
 }]);
 
