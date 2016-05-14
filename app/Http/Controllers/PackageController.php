c908ce195c75753d1f09ee730569d091829e8581
CGTAbhay <abhay@cgt.co.in>
2016-05-14 11:00:46 +0530

    Party package update  in frontend

--- app/Http/Controllers/PackageController.php
+++ app/Http/Controllers/PackageController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use AlcoholDelivery\Http\Requests;
 use AlcoholDelivery\Packages;
+use AlcoholDelivery\Setting;
+use AlcoholDelivery\Categories;
 use AlcoholDelivery\Http\Controllers\Controller;
 
 class PackageController extends Controller
@@ -92,6 +94,57 @@ class PackageController extends Controller
 
         $packages = $packages->with('productlist')->where('type',1)->get();
 
+        if($packages){
+            
+            $settingObj = new Setting;
+            
+            $global = $settingObj->getSettings(array(
+              "key"=>'pricing',
+              "multiple"=>false
+            ));
+
+            foreach ($packages as $key => $package) {
+                
+
+                $packageItems = $package['packageItems'];
+                $packageupdate = [];
+                foreach ($packageItems as $pkgkey => $pkgvalue) {              
+                  $packageupdate[$pkgkey] = $pkgvalue;
+                  $pkgpro = [];
+                  foreach ($pkgvalue['products'] as $prokey => $provalue) {
+                      $tier = $global->settings['regular_express_delivery'];
+                      $value = $this->getProductById($provalue['_id'], $package['productlist']);     
+
+                      if(isset($value['regular_express_delivery']) && !empty($value['regular_express_delivery'])){
+                      $tier = $value['regular_express_delivery'];          
+                      }else{
+                        $categories = Categories::whereIn('_id',$value['categories'])->get();
+                        if($categories){
+                          foreach ($categories as $ckey => $cvalue) {
+                            if(isset($cvalue['regular_express_delivery']) && !empty($cvalue['regular_express_delivery'])){
+                              $tier = $cvalue['regular_express_delivery'];                
+                            }
+                          }
+                        }
+                      }
+                      $sprice = $this->calculatePrice($value['price'],$tier);
+
+                      $pkgpro[$prokey] = [
+                        '_id' => $provalue['_id'],
+                        'cprice' => $provalue['cprice'],
+                        'name' => $value['name'],
+                        'sprice' => $sprice,
+                        'imageFiles' => $value['imageFiles']
+                      ];
+                  }
+                  $packageupdate[$pkgkey]['products'] = $pkgpro;              
+                }
+                
+                $packages[$key]->packageItems = $packageupdate;
+                //$package->packageItems = $packageupdate;
+            }
+        }
+
         return response($packages,200);
 
     }
@@ -102,7 +155,28 @@ class PackageController extends Controller
 
         $packages = $packages->with('productlist')->where('type',2)->get();
 
+
         return response($packages,200);
 
+    }
+
+    protected function calculatePrice($cost = 0, $tiers){
+      if($tiers['type'] == 1){
+        $p = $cost+($cost/100*$tiers['value']);
+      }else{
+        $p = $cost+$tiers['value'];
+      }      
+      return round($p,2);
+    }
+
+    protected function getProductById($id, $parray){
+        $return = [];
+        foreach ($parray as $key => $value) {
+            if($value['_id'] == $id){
+                $return = $value;
+                break;
+            }
+        }
+        return $return;
     }
 }
