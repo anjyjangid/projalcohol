c519161010bfdb2f4d95d745a9a14968bc426426
CGTAbhay <abhay@cgt.co.in>
2016-06-25 16:28:59 +0530

    Sidebar in mobile

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -51,7 +51,7 @@
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-
+		<!-- <link rel="stylesheet" media="screen and (max-device-width: 480px)" type="text/css" href="css/slidebars.css"> -->
 		<link rel="shortcut icon" href="images/favicon.ico"/>		
 	</head>
 	<body ng-controller="AppController">
@@ -246,4 +246,6 @@
     <script type="text/javascript" src="js/directive.js"></script>
     <script src="{{ asset('bower_components/angular-fblogin/dist/angular-fblogin.js') }}"></script>   
     <script src="{{ asset('bower_components/v-accordion/dist/v-accordion.js') }}"></script>       
+    <!-- <script type="text/javascript" src="js/slidebars.js"></script> -->
+
 </html>
