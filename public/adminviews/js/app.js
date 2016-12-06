00111227f50c44ca767b9f2d593c4416d43fe475
CGTAnuragPareek <anurag.pareek@cgt.co.in>
2016-12-06 10:25:38 +0530

    Fix admin order minor issue

--- public/adminviews/js/app.js
+++ public/adminviews/js/app.js
@@ -1545,7 +1545,7 @@ MetronicApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
 
 				"" : {
 					templateUrl: "adminviews/views/orders/order/index.html",
-					controller : "OrderCreateControllerOrderCreateController",
+					controller : "OrderCreateController",
 				},
 				"products@userLayout.orders.consumer" : {
 					templateUrl: "adminviews/views/orders/order/products.html",
