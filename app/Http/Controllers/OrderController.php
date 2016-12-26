40cae1791d58b9eec6d76556129c0263cd164136
CGTAbhay <abhay@cgt.co.in>
2016-12-26 15:40:18 +0530

    Check for reference

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -215,6 +215,7 @@ class OrderController extends Controller
 
 	public function getOrderdetail(Request $request,$reference){
 
+		//return strtotime('now').' = '.(round(microtime(true) * 1000));
 		$order = Orders::where('reference','=',$reference)->first();		
 
 
