8494798208f145352fbc06fecde43a8b60c70c93
CGTAbhay <abhay@cgt.co.in>
2016-12-27 10:41:43 +0530

    Email to static remove

--- app/Email.php
+++ app/Email.php
@@ -296,7 +296,7 @@ class Email extends Moloquent
 				/*LAYOUT BASED MAIL*/
 
 				$data = ['content' => $this->recipient_info['message'],'replace'=>$this->recipient_info['replace']];
-				$this->recipient_info["receiver"]['email'] = 'anuragcgt@gmail.com';
+				//$this->recipient_info["receiver"]['email'] = 'anuragcgt@gmail.com';
 
 				Mail::send('emails.mail', $data, function ($message) {
 					$message->setTo(array($this->recipient_info["receiver"]['email']=>$this->recipient_info["receiver"]['name']));
