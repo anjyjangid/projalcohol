37c50dff9bf3547c4f7d2f6dec14ed913f2b166f
CGTAbhay <abhay@cgt.co.in>
2016-12-30 19:07:36 +0530

    Minor fix

--- public/js/directive.js
+++ public/js/directive.js
@@ -118,19 +118,18 @@ AlcoholDelivery.directive('sideBar', function() {
 
 				FB.login(function(response) {
 				    if (response.authResponse) {				     
-				     FB.api('/me', {fields: 'first_name,last_name,locale,email,birthday'},function(response) {
+				     FB.api('/me', {fields: 'first_name,last_name,locale,email,birthday'},function(result) {
 				       	$mdDialog.hide();
-						$http.post('/auth/registerfb',response)
+						$http.post('/auth/registerfb',result)
 						.success(function(res){
 							$scope.loginSuccess(res);
-
-						}).error(function(result){
-							$scope.socialError = result;
+						}).error(function(erresult){
+							$scope.socialError = erresult;
 							$scope.signupOpen();
 						});
 				     });
 				    } else {
-				     console.log('User cancelled login or did not fully authorize.');
+				     	alert(JSON.parseInt(response));
 				    }
 				});
 
@@ -342,52 +341,6 @@ AlcoholDelivery.directive('sideBar', function() {
 		        });
 		    }
 
-		    watchLoginChange = function() {
-
-			  var _self = this;
-
-			  FB.Event.subscribe('auth.authResponseChange', function(res) {
-
-			    if (res.status === 'connected') {
-
-			      /*
-			       The user is already logged,
-			       is possible retrieve his personal info
-			      */
-			      _self.getUserInfo();
-
-			      /*
-			       This is also the point where you should create a
-			       session for the current user.
-			       For this purpose you can use the data inside the
-			       res.authResponse object.
-			      */
-
-			    }
-			    else {
-
-			      /*
-			       The user is not logged to the app, or into Facebook:
-			       destroy the session on the server.
-			      */
-
-			    }
-
-			  });
-
-			}
-
-			getUserInfo = function() {
-
-			  var _self = this;
-
-			  FB.api('/me', function(res) {
-			    $rootScope.$apply(function() {
-			      $rootScope.user = _self.user = res;
-			    });
-			  });
-
-			}
 		}
 	};
 })
