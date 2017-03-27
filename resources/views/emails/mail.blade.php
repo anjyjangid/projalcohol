2d006cee45a5cd900f1bb73d4d5630d56d8c5e76
CGTAbhay <abhay@cgt.co.in>
2017-03-27 11:17:00 +0530

    UI fixes by mukesh sir

--- resources/views/emails/mail.blade.php
+++ resources/views/emails/mail.blade.php
@@ -1,4 +1,31 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Email Template</title>
+   <style type="text/css">
+        table td {
+            border-collapse: collapse !important;
+        }
+        @media only screen and (max-width: 599px) {
+            table[class="flexible"] {
+                width: 100% !important;
+            }
+            td[class="logo"] {
+                text-align: center;
+            }
+            td[class="logo"] img {
+                height: 60px !important;
+            }            
+        }
+    </style>
+</head>
+<body style="margin: 0; padding: 0; background-color: #ebeef0;">
 @extends('emails.maillayout')
 @section('content')
 <?php echo htmlspecialchars_decode($content); ?>
 @endsection
+</body>
+</html>
